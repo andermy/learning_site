a47df9e6ad7fc62aa061454836fa31de5b31d2b4
Anders <andermy@gmail.com>
2017-02-06 10:50:53 +0100

    Add view

--- courses/views.py
+++ courses/views.py
@@ -1,28 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import generics
 
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from . import models
 from . import serializers
-from .models import Course
 
-class ListCreateCourse(APIView):
-    def get(self, request, format=None):
-        courses = models.Course.objects.all()
-        serializer = serializers.CourseSerializer(courses, many=True)
-        return Response(serializer.data)
+class ListCreateCourse(generics.ListCreateAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
 
-    def post(self, request, format=None):
-        serializer = serializers.CourseSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-# Create your views here.
-def course_list(request):
-    courses = Course.objects.all()
-    output = ', '.join([str(course) for course in courses])
-    return HttpResponse(output)
+class RetrieveUpdateDestroyCourse(generics.RetrieveDestroyAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
